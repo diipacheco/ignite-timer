4ae24890073da20779767232f0fb12b8e3fb9012
diipacheco <edilsonpacheco6@gmail.com>
2024-05-28 15:58:55 -0300

    Finish Home page styles

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,57 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import { Play } from 'phosphor-react';
+import {
+  CountDownContainer,
+  FormContainer,
+  HomeContainer,
+  Separator,
+  StartCountDownButton,
+  TaskInput,
+  MinutesAmountInput,
+} from './styles';
+
+export function Home() {
+  return (
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <TaskInput list="task-suggestions" placeholder="Dê um nome para o seu projeto" id="task" />
+
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+            <option value="Projeto 1" />
+          </datalist>
+
+          <label htmlFor="minutesAmount">durante</label>
+          <MinutesAmountInput
+            placeholder="00"
+            type="number"
+            id="minutesAmount"
+            step={5}
+            min={5}
+            max={60}
+          />
+
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CountDownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountDownContainer>
+
+        <StartCountDownButton disabled type="submit">
+          <Play size={24} />
+          Começar
+        </StartCountDownButton>
+      </form>
+    </HomeContainer>
+  );
+}
