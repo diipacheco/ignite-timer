23b8c2c703ab3b110e820e23eb579e17f15801fb
diipacheco <edilsonpacheco6@gmail.com>
2024-06-04 12:13:40 -0300

    Adds timer counting implementation

---
+++ src/pages/Home/components/NewCycleForm/index.tsx
@@ -0,0 +1,38 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+/* eslint-disable jsx-a11y/control-has-associated-label */
+import { FormContainer, MinutesAmountInput, TaskInput } from './styles';
+
+export function NewCycleForm() {
+  return (
+    <FormContainer>
+      <label htmlFor="task">Vou trabalhar em</label>
+      <TaskInput
+        list="task-suggestions"
+        placeholder="Dê um nome para o seu projeto"
+        id="task"
+        {...register('task')}
+        disabled={!!activeCycle}
+      />
+
+      <datalist id="task-suggestions">
+        <option value="Projeto 1" />
+        <option value="Projeto 2" />
+        <option value="Projeto 3" />
+        <option value="Projeto 1" />
+      </datalist>
+
+      <label htmlFor="minutesAmount">durante</label>
+      <MinutesAmountInput
+        placeholder="00"
+        type="number"
+        id="minutesAmount"
+        step={5}
+        min={5}
+        max={60}
+        {...register('minutesAmount', { valueAsNumber: true })}
+        disabled={!!activeCycle}
+      />
+      <span>minutos.</span>
+    </FormContainer>
+  );
+}
